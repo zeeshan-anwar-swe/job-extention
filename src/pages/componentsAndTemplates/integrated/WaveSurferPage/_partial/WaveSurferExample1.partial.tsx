868cc17534e2c1b2021a5446fcfa701f43dcda9d
Zeeshan Anwar <zeeshan.anwar.swe@gmail.com>
2025-02-11 18:58:34 +0500

    Removed unnessary pages

--- src/pages/componentsAndTemplates/integrated/WaveSurferPage/_partial/WaveSurferExample1.partial.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import WaveSurferPlayer from '../../../../../components/WaveSurferPlayer';
-
-const WaveSurferExample1Partial = () => {
-	// return <WaveSurferPlayer url='/example-sound.mp3' container='div' />;
-	return <div>WaveSurferExample1Partial</div>;
-};
-
-export default WaveSurferExample1Partial;
