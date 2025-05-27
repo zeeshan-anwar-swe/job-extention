a59680ab9927563bb464dc90a600af376bbab35d
Zeeshan Anwar <zeeshan.anwar.swe@gmail.com>
2025-05-27 14:36:26 +0500

    chart issue fixed

--- src/modules/AgencyAdmin.module/pages/DashboardPage/_partial/Chart.partial.tsx
+++ src/modules/AgencyAdmin.module/pages/DashboardPage/_partial/Chart.partial.tsx
@@ -1,63 +1,108 @@
 import React, { useState } from 'react';
 import Card, {
-  CardBody,
-  CardHeader,
-  CardHeaderChild,
-  CardTitle,
+	CardBody,
+	CardHeader,
+	CardHeaderChild,
+	CardSubTitle,
+	CardTitle,
 } from '../../../../../components/ui/Card';
 import ReactApexChart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
+import colors from 'tailwindcss/colors';
 
-const ChartPartial = ({series, categories}:{series:ApexOptions["series"], categories:string[]}) => {
-  console.log({series, categories});
+const ChartPartial = ({
+	series,
+	categories,
+}: {
+	series: ApexOptions['series'];
+	categories: string[];
+}) => {
+	const [state, setState] = React.useState({
+		series: series,
+		options: {
+			colors: [
+				colors.blue['500'],
+				colors.violet['500'],
+				colors.emerald['500'],
+				colors.amber['500'],
+				colors.rose['500'],
+				colors.purple['500'],
+			],
+			chart: {
+				height: 400,
+				type: 'line',
+				zoom: {
+					enabled: false,
+				},
+			},
 
-    const [state, setState] = React.useState({
+      plotOptions: {
+				bar: {
+					horizontal: false,
+					columnWidth: '55%',
+					borderRadiusWhenStacked: 'all',
+				},
+			},
 
-          series: series,
-          options: {
-            chart: {
-              height: 400,
-              type: 'line' as const,
-              zoom: {
-                enabled: false
-              }
-            },
+			dataLabels: {
+				enabled: false,
+			},
 
-            dataLabels: {
-              enabled: false
-            },
-            stroke: {
-              curve: 'smooth' as ApexOptions["stroke"]["curve"]
-            },
-            title: {
-              text: 'Product Trends by Month',
-              align: 'left' as ApexOptions["title"]["align"] // Explicitly type 'left'
-            },
-            grid: {
-              row: {
-                colors: ['#f3f3f3', 'transparent'],
-                opacity: 0.5
-              },
-            },
-            xaxis: {
-              categories: categories,
-            }
-          },
+			stroke: {
+				curve: 'smooth',
+        show: true,
+				// width: 2,
+				// colors: ['transparent'],
+			},
+			title: {
+				// text: 'Job Openings vs Filled',
+				align: 'left', // Explicitly type 'left'
+			},
+			grid: {
+				show: true,
+				borderColor: `${colors.zinc['500']}25`,
+				strokeDashArray: 0,
+				xaxis: {
+					lines: {
+						show: false,
+					},
+				},
+				yaxis: {
+					lines: {
+						show: true,
+					},
+				},
 
-
-      });
-  return (
-    <Card className='h-full'>
-      <CardHeader>
-        <CardHeaderChild>
-          <CardTitle>Chart</CardTitle>
-        </CardHeaderChild>
-      </CardHeader>
-      <CardBody>
-        <ReactApexChart options={state.options} series={state.series} type="line" height={400} />
-      </CardBody>
-    </Card>
-  );
+				padding: {
+					top: 0,
+					right: 10,
+					bottom: 0,
+					left: 10,
+				},
+			},
+			xaxis: {
+				categories: categories,
+			},
+		} as ApexOptions,
+	});
+	return (
+		<Card className='h-full'>
+			<CardHeader>
+				<CardHeaderChild className='!block'>
+					<CardSubTitle>Performance Metrics</CardSubTitle>
+					<CardTitle>Job Openings vs Filled</CardTitle>
+				</CardHeaderChild>
+			</CardHeader>
+			<CardBody>
+				<ReactApexChart
+					options={state.options}
+					series={state.series}
+					type='line'
+					height={400}
+				/>
+			</CardBody>
+		</Card>
+	);
 };
 
 export default ChartPartial;
